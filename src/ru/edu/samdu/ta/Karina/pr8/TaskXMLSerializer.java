62e0beafd0625af9002117d300de08c753b9eb83
Karina <demi4815@gmail.com>
2020-11-29 19:33:52 +0300

    Version (1)

--- src/ru/edu/samdu/ta/Karina/pr8/TaskXMLSerializer.java
+++ src/ru/edu/samdu/ta/Karina/pr8/TaskXMLSerializer.java
@@ -44,35 +44,77 @@ public class TaskXMLSerializer
         XMLOutputFactory output = XMLOutputFactory.newInstance();
         XMLStreamWriter writer = output.createXMLStreamWriter(new FileWriter(fileName));
 
-        // Открываем XML-документ и Пишем корневой элемент BookCatalogue
-        writer.writeStartDocument("1.0");
+        writer.writeStartDocument("UTF-8","1.0");
+
         writer.writeStartElement("tasks");
 
         for (int i = 0; i < object.size(); i++) {
 
             writer.writeStartElement("task");
 
-            writer.writeAttribute("active", String.valueOf(object.getTask(i).active));
+            if (object.getTask(i).isRepeated())
+            {
+                writer.writeAttribute("end", String.valueOf(object.getTask(i).end));
 
-            writer.writeAttribute("end", String.valueOf(object.getTask(i).end));
+                writer.writeAttribute("repeat", String.valueOf(object.getTask(i).repeat));
 
-            writer.writeAttribute("repeat", String.valueOf(object.getTask(i).repeat));
+                writer.writeAttribute("start", String.valueOf(object.getTask(i).start));
+            }
+            else
+            {
+                writer.writeAttribute("end", "");
+
+                writer.writeAttribute("repeat", "0");
+
+                writer.writeAttribute("start", "");
+            }
+
+            writer.writeAttribute("time", String.valueOf(object.getTask(i).time));
 
             writer.writeAttribute("repeated", String.valueOf(object.getTask(i).isRepeated()));
 
-            writer.writeAttribute("start", String.valueOf(object.getTask(i).start));
-
-            writer.writeAttribute("time", String.valueOf(object.getTask(i).time));
+            writer.writeAttribute("active", String.valueOf(object.getTask(i).active));
 
             writer.writeCharacters(object.getTask(i).title);
 
             writer.writeEndElement();
         }
-        // Закрываем корневой элемент
-        writer.writeEndElement();
-        // Закрываем XML-документ
-        writer.writeEndDocument();
-        writer.flush();
 
+        writer.writeEndElement();
+
+        writer.writeEndDocument();
+
+        writer.flush();
+    }
+
+    public static void test() throws IOException, XMLStreamException
+    {
+        final String fileName = "tasks.xml";
+        final String resultName1 = "result1.xml";
+        final String resultName2 = "result2.xml";
+
+        TaskXMLSerializer.load(fileName);
+
+        Task[] tasks = {
+                new Task("A",0),
+                new Task("B",1),
+                new Task("C", 2, 10, 1),
+                new Task("D", 3, 21, 3)
+        };
+
+        tasks[0].setActive(true);
+        tasks[2].setActive(true);
+
+        AbstractTaskList list1 =  new ArrayTaskList();
+        AbstractTaskList list2 =  new LinkedTaskList();
+
+        for (int i = 0; i < tasks.length; i++)
+        {
+            list1.add(tasks[i]);
+            list2.add(tasks[i]);
+        }
+
+        TaskXMLSerializer.save(list1, resultName1);
+        TaskXMLSerializer.save(list2, resultName2);
     }
 }
--- src/ua/edu/sumdu/ta/Karina/pr8/Main.java
+++ src/ua/edu/sumdu/ta/Karina/pr8/Main.java
@@ -3,41 +3,12 @@ package ua.edu.sumdu.ta.Karina.pr8;
 import ru.edu.samdu.ta.Karina.pr8.*;
 
 import javax.xml.stream.XMLStreamException;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, XMLStreamException {
-
-        final String fileName = "tasks.xml";
-        final String resultName1 = "result1.xml";
-        final String resultName2 = "result2.xml";
-
-        TaskXMLSerializer.load(fileName);
-
-        Task[] tasks = {
-                new Task("A",0),
-                new Task("B",1),
-                new Task("C", 2, 10, 1),
-                new Task("D", 3, 21, 3)
-        };
-
-        tasks[0].setActive(true);
-        tasks[2].setActive(true);
-
-        AbstractTaskList list1 =  new ArrayTaskList();
-        AbstractTaskList list2 =  new LinkedTaskList();
-
-        for (int i = 0; i < tasks.length; i++)
-        {
-            list1.add(tasks[i]);
-            list2.add(tasks[i]);
-        }
-
-        TaskXMLSerializer.save(list1, resultName1);
-        TaskXMLSerializer.save(list2, resultName2);
-
-
+    public static void main(String[] args) throws IOException, XMLStreamException
+    {
+        TaskXMLSerializer.test();
     }
 }
